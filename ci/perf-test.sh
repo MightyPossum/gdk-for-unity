588986aded40eb56adfc082b45ee5a08e2dc1234
Paul Balaji <paulbalaji@improbable.io>
2020-06-16 16:30:13 +0100

    woops
    Signed-off-by: Paul Balaji <paulbalaji@improbable.io>

--- ci/perf-test.sh
+++ ci/perf-test.sh
@@ -18,50 +18,6 @@ mkdir -p "${TEST_RESULTS_DIR}"
 
 TEST_SETTINGS_DIR="${PROJECT_DIR}/workers/unity/Packages/io.improbable.gdk.testutils/TestSettings"
 
-traceStart "Testing Unity: Editmode :writing_hand:"
-    pushd "workers/unity"
-        traceStart "Burst default"
-            dotnet run -p "${PROJECT_DIR}/.shared-ci/tools/RunUnity/RunUnity.csproj" -- \
-                -batchmode \
-                -projectPath "${PROJECT_DIR}/workers/unity" \
-                -runEditorTests \
-                -testCategory "Performance" \
-                -logfile "${PROJECT_DIR}/logs/editmode-burst-default-perftest-run.log" \
-                -editorTestsResultFile "${TEST_RESULTS_DIR}/editmode-burst-default-perftest-results.xml" \
-                "${ACCELERATOR_ARGS}"
-        traceEnd
-
-        traceStart "Burst disabled"
-            dotnet run -p "${PROJECT_DIR}/.shared-ci/tools/RunUnity/RunUnity.csproj" -- \
-                -batchmode \
-                -projectPath "${PROJECT_DIR}/workers/unity" \
-                -runEditorTests \
-                -testCategory "Performance" \
-                -logfile "${PROJECT_DIR}/logs/editmode-burst-disabled-perftest-run.log" \
-                -editorTestsResultFile "${TEST_RESULTS_DIR}/editmode-burst-disabled-perftest-results.xml" \
-                "${ACCELERATOR_ARGS}" \
-                --burst-disable-compilation
-        traceEnd
-    popd
-traceEnd
-
-traceStart "Testing Unity: Playmode :joystick:"
-    pushd "workers/unity"
-        for burst in burst-default burst-disabled
-        do
-            for scriptingBackend in mono il2cpp winrt
-            do
-                for apiProfile in dotnet-std-2 dotnet-4
-                do
-                    runPlaymodeTests $burst $scriptingBackend $apiProfile "Performance" "StandaloneWindows64"
-                done
-            done
-        done
-    popd
-traceEnd
-
-cleanUnity "$(pwd)/workers/unity"
-
 function runPlaymodeTests {
     local burst=$1
     local scriptingBackend=$2
@@ -88,3 +44,47 @@ function runPlaymodeTests {
             "${args[@]}"
     traceEnd
 }
+
+traceStart "Performance Testing: Editmode :writing_hand:"
+    pushd "workers/unity"
+        traceStart "Burst default"
+            dotnet run -p "${PROJECT_DIR}/.shared-ci/tools/RunUnity/RunUnity.csproj" -- \
+                -batchmode \
+                -projectPath "${PROJECT_DIR}/workers/unity" \
+                -runEditorTests \
+                -testCategory "Performance" \
+                -logfile "${PROJECT_DIR}/logs/editmode-burst-default-perftest-run.log" \
+                -editorTestsResultFile "${TEST_RESULTS_DIR}/editmode-burst-default-perftest-results.xml" \
+                "${ACCELERATOR_ARGS}"
+        traceEnd
+
+        traceStart "Burst disabled"
+            dotnet run -p "${PROJECT_DIR}/.shared-ci/tools/RunUnity/RunUnity.csproj" -- \
+                -batchmode \
+                -projectPath "${PROJECT_DIR}/workers/unity" \
+                -runEditorTests \
+                -testCategory "Performance" \
+                -logfile "${PROJECT_DIR}/logs/editmode-burst-disabled-perftest-run.log" \
+                -editorTestsResultFile "${TEST_RESULTS_DIR}/editmode-burst-disabled-perftest-results.xml" \
+                "${ACCELERATOR_ARGS}" \
+                --burst-disable-compilation
+        traceEnd
+    popd
+traceEnd
+
+traceStart "Performance Testing: Playmode :joystick:"
+    pushd "workers/unity"
+        for burst in burst-default burst-disabled
+        do
+            for scriptingBackend in mono il2cpp winrt
+            do
+                for apiProfile in dotnet-std-2 dotnet-4
+                do
+                    runPlaymodeTests $burst $scriptingBackend $apiProfile "Performance" "StandaloneWindows64"
+                done
+            done
+        done
+    popd
+traceEnd
+
+cleanUnity "$(pwd)/workers/unity"
